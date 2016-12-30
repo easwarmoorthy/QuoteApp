4c904e49835342978a6254fa341a38ea298c1195
Easwar Moorthy <easwarvem@gmail.com>
2016-12-30 22:22:44 +0530

    Create Project

--- First_Project/First_Project/urls.py
+++ First_Project/First_Project/urls.py
@@ -1,4 +1,4 @@
-"""Polls URL Configuration
+"""First_Project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
